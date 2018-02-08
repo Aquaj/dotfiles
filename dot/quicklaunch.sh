fda22a96a2cf8703fb4cd8c7130e236190a4ec34
Jérémie Bonal <jeremie.bonal@gmail.com>
2018-02-08 19:41:24 +0100

    Removed now useless programs from quicklaunch.

--- dot/quicklaunch.sh
+++ dot/quicklaunch.sh
@@ -5,20 +5,13 @@ browser() {
 }
 
 communication() {
-  discord&
   slack&
-  thunderbird&
   messengerfordesktop&
-}
-
-files() {
-  seadrive-gui&
 }
 
 quicklaunch() {
   browser 2>/dev/null;
   communication 2> /dev/null;
-  files 2> /dev/null;
 }
 
 alias ql=quicklaunch
